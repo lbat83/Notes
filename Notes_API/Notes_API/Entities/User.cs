373bc5fc3d08da9f238e83b3ce593aa3ee2bad7b
lbat83 <lbz1983@gmail.com>
2018-04-06 22:52:07 -0500

    Assignment 7

---
+++ Notes_API/Notes_API/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notes_API.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public DateTime? CreatedOn { get; set; }
+
+        public ICollection<Notes> Notes { get; set; }
+    }
+}
