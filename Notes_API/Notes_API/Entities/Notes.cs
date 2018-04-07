ca504ba31390dd28d53a6cb2a847559665154a81
lbat83 <lbz1983@gmail.com>
2018-04-06 23:44:37 -0500

    Assignment 7

--- Notes_API/Notes_API/Entities/Notes.cs
+++ Notes_API/Notes_API/Entities/Notes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace Notes_API.Entities
 {
     public class Notes
     {
+        [Key]
         public int Id { get; set; }
         public string Title { get; set; }
         public string Note { get; set; }
--- Notes_API/Notes_API/Entities/User.cs
+++ Notes_API/Notes_API/Entities/User.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Notes_API.Entities
 {
     public class User
-    {
+    {   [Key]
         public int Id { get; set; }
         public string Email { get; set; }
         public string Name { get; set; }
